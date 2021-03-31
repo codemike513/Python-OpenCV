5718d9a19353fd4afacc9ed6dd743fe63db80334
Mihir Pesswani <pesswani.mihir@gmail.com>
2021-04-01 00:26:47 +0530

    Chapter 1

---
+++ Chapter 1.2.py
@@ -0,0 +1,16 @@
+# READ IMAGES - VIDEO - WEBCAM
+# 1 -> READ VIDEO
+
+import cv2
+
+# For Reading Video we have to create a VideCapture Object in its parameter we give the path of video
+# cap = cv2.VideoCapture("Video Location")
+cap = cv2.VideoCapture("Resources/abc.mp4")
+
+# Since Video is a Sequence of images we have to use a loop for display
+while True:
+    success, img = cap.read()    # The success variable is boolean type it captures the image if true
+    cv2.imshow("Video", img)
+    # Now we add a delay and wait for 'q' press to break the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
