b6b042970a8214f7ec0fe5c1c1728b8d13ed5a2c
Mihir Pesswani <pesswani.mihir@gmail.com>
2021-04-01 00:27:56 +0530

    Chapter 1

--- Chapter 1.2.py
+++ Chapter 1.2.py
@@ -1,5 +1,5 @@
 # READ IMAGES - VIDEO - WEBCAM
-# 1 -> READ VIDEO
+# 1.2 -> READ VIDEO
 
 import cv2
 
@@ -9,7 +9,8 @@ cap = cv2.VideoCapture("Resources/abc.mp4")
 
 # Since Video is a Sequence of images we have to use a loop for display
 while True:
-    success, img = cap.read()    # The success variable is boolean type it captures the image if true
+    # The success variable is boolean type it captures the image if true
+    success, img = cap.read()
     cv2.imshow("Video", img)
     # Now we add a delay and wait for 'q' press to break the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
